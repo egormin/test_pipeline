d3ca551b569f0a10ecf8b864a8889b1dc6868bcd
egormin <neoman2014@gmail.com>
2018-09-09 00:11:18 +0300

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -17,6 +17,14 @@ resource "aws_security_group" "allow_ssh" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+    
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
 
   tags {
     Name = "allow_ssh"
